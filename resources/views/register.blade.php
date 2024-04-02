4e8b76aedc867734fd7e9afd6161d239a5b3e888
MartinH2k3 <martinko.horsky.2003@gmail.com>
2024-04-02 14:15:57 +0200

    Forgot to add main css to register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
 <body>
